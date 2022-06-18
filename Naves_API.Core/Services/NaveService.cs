200763864a83597e076db1d25ab13a443a36082d
Brian <brianblanquiceth48@gmail.com>
2022-06-18 14:19:35 -0500

    se termina servicios

---
+++ Naves_API.Core/Services/NaveService.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Naves_API.Core.Entities;
+using Naves_API.Core.Interfaces;
+using Naves_API.Infrastructure.Core;
+
+namespace Naves_API.Core.Services
+{
+
+
+    public class NaveService : INaveService
+    {
+        private readonly INaveRepository _naveRepository;// inyeccion de repositorio
+        private readonly IStockRepository _stock;// inyeccion de repositorio, stcok
+        public NaveService(INaveRepository naveRepository, IStockRepository  stock)
+        {
+
+            _naveRepository = naveRepository;
+            _stock = stock;
+        }
+
+        public async  Task<IEnumerable<Nave>> GetNaves()
+        {
+            return await _naveRepository.GetNaves();
+        }
+
+        public async Task<Nave> GetNaves(int id)
+        {
+            return await _naveRepository.GetNaves(id);
+        }
+
+        public async Task<IEnumerable<Nave>> GetNavesLike(string buscar)
+        {
+           return  await _naveRepository.GetNavesLike(buscar);
+        }
+
+        public async Task InsertNave(Nave nave)
+        {
+
+            var stock = _stock.Stocks(nave.TipoNave);
+
+            if (stock > 2)
+            {
+                throw new Exception("lleno el stock");
+            }
+
+            await _naveRepository.InsertNave(nave);
+        }
+    }
+}
--- Naves_API/Controllers/NavesController.cs
+++ Naves_API/Controllers/NavesController.cs
@@ -17,19 +17,19 @@ namespace Naves_API.Controllers
   
     public class NavesController : ControllerBase
     {
-        readonly INaveRepository _naveRepository;// inyeccion de repositorio
+        readonly INaveService _naveService;// inyeccion de repositorio
         private readonly IMapper _mapper; // inyeccion de dtos
 
-        public NavesController(INaveRepository naveRepository, IMapper mapper)
+        public NavesController(INaveService naveService, IMapper mapper)
         {
-            _naveRepository = naveRepository;
+            _naveService = naveService;
             _mapper = mapper;
         }
 
         [HttpGet]
         public async Task <IActionResult> GetNaves()
         {
-            var naves = await _naveRepository.GetNaves();
+            var naves = await _naveService.GetNaves();
             var navesdots = _mapper.Map<IEnumerable<NaveDto>>(naves);
 
             return Ok(navesdots);
@@ -39,7 +39,7 @@ namespace Naves_API.Controllers
         [HttpGet("{id}")]
         public async Task<IActionResult> GetNaves(int id)
         {
-            var nave = await _naveRepository.GetNaves(id);
+            var nave = await _naveService.GetNaves(id);
             var navedto = _mapper.Map<NaveDto>(nave);
 
             return Ok(navedto);
@@ -50,7 +50,7 @@ namespace Naves_API.Controllers
         public async Task<IActionResult> Nave(NaveDto nave)
         {
             var navedto = _mapper.Map<Nave>(nave);
-            await _naveRepository.InsertNave(navedto);
+            await _naveService.InsertNave(navedto);
 
             return Ok(nave);
 
@@ -60,7 +60,7 @@ namespace Naves_API.Controllers
         public async Task<IActionResult> GetNaves(string buscar)
         {
             
-            var nave = await _naveRepository.GetNavesLike(buscar);
+            var nave = await _naveService.GetNavesLike(buscar);
             var navedto = _mapper.Map<IEnumerable<NaveDto>>(nave);
             return Ok(navedto);
 
